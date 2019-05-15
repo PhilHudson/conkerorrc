c6d4f3671b76bd2c6ebd6845b71bdf8b96d3e87d
Phil Hudson <phil.hudson@iname.com>
2019-05-15 13:18:33 +0100

    Leave media handler settings to OS-specific code.

--- 11protocol-handlers.js
+++ 11protocol-handlers.js
@@ -1,4 +1,4 @@
-// Time-stamp: "2017-08-27 17:02:22 phil"
+// Time-stamp: "2019-01-19 20:19:15 phil"
 
 dumpln("11protocol-handlers");
 
@@ -17,5 +17,7 @@ set_protocol_handler("mailto",
 // git -> git clone
 set_protocol_handler("git", make_file("~/bin/git-uri-clone"));
 
-external_content_handlers.set("video/*", "vlc");
-external_content_handlers.set("application/pdf", "acroread");
+// Leave video handler to OS-specific code
+// external_content_handlers.set("video/*", "vlc");
+// Leave PDF handler to OS-specific code
+// external_content_handlers.set("application/pdf", "acroread");
