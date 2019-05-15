92b9da81de13016ce7bbdf919eb12366d8b2753e
Phil Hudson <phil.hudson@iname.com>
2019-05-15 13:05:37 +0100

    Set up handlers for shell-command-on-url.

--- linux/50os.linux.js
+++ linux/50os.linux.js
@@ -1,12 +1,13 @@
-// Time-stamp: "2018-04-14 21:07:05 phil"
+// Time-stamp: "2019-01-27 00:55:30 phil"
 
 dumpln("50os.linux");
 
 content_handlers.set("application/pdf", content_handler_save);
 
 external_content_handlers.set("image/*", "xdg-open");
-external_content_handlers.set("video/*", "xdg-open");
-external_content_handlers.set("audio/*", "xdg-open");
+external_content_handlers.set("video/*", "yd2tv");
+external_content_handlers.set("audio/*", "yd2tv");
+external_content_handlers.set("application/octet-stream", "yd2tv");
 external_content_handlers.set("application/pdf", "xdg-open");
 external_content_handlers.set("application/postscript", "xdg-open");
 external_content_handlers.set("application/x-dvi", "xdg-open");
