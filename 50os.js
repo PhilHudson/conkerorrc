adf6b75e5c9fc5014363f538036edb86f3671ab6
Phil Hudson <phil.hudson@iname.com>
2019-05-15 13:07:58 +0100

    Fixed OS-specific loading.

--- 50os.js
+++ 50os.js
@@ -1,1 +1,6 @@
-load(getenv("OSTYPE") + "/50os." + getenv("OSTYPE") + ".js");
+dumpln('50os');
+
+let os_type = getenv("OSTYPE");
+let config_dir = get_home_directory().clone();
+config_dir.appendRelativePath(`.conkerorrc/${os_type}/`);
+load(make_file(`${config_dir.path}/50os.${os_type}.js`));
